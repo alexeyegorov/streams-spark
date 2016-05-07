12070132e8ab7f21d9bf5ae54479f04b86462bfd
Alexey Egorov <aegorov87@gmail.com>
2016-05-07 15:14:14 +0200

    add constant

--- src/main/java/stream/Constants.java
+++ src/main/java/stream/Constants.java
@@ -11,4 +11,5 @@ public class Constants {
     public static final String SPARK_BATCH_INTERVAL = "spark-batch-interval";
 
     public static final int DEFAULT_PARALLELISM = 1;
+    public static final String SPARK_MASTER_NODE = "spark-master-node";
 }
