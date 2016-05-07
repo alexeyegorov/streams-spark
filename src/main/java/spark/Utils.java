bf80f9371491c4c91deda389331c876f160bb8e6
Alexey Egorov <aegorov87@gmail.com>
2016-05-07 15:09:31 +0200

    remove streamtopology class and extract static method into Utils class

--- src/main/java/spark/Utils.java
+++ src/main/java/spark/Utils.java
@@ -1,5 +1,8 @@
 package spark;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -7,10 +10,16 @@ import org.w3c.dom.NodeList;
 import java.util.ArrayList;
 import java.util.List;
 
+import stream.runtime.DependencyInjection;
+import stream.runtime.setup.handler.PropertiesHandler;
+import stream.util.Variables;
+
 /**
  * @author alexey
  */
 public class Utils {
+
+    static Logger log = LoggerFactory.getLogger(Utils.class);
 
     /**
      * Go recursively through all children of each element and check if they have 'queue' or
@@ -40,5 +49,32 @@ public class Utils {
             }
         }
         return output;
+    }
+
+    /**
+     * Handle properties in XML file and add them to variables.
+     *
+     * @param doc       XML file as document
+     * @param variables map of variables
+     */
+    public static Variables handleProperties(Document doc, Variables variables) {
+        DependencyInjection dependencies = new DependencyInjection();
+        try {
+            PropertiesHandler handler = new PropertiesHandler();
+            handler.handle(null, doc, variables, dependencies);
+
+            if (log.isDebugEnabled()) {
+                log.debug("########################################################################");
+                log.debug("Found properties: {}", variables);
+                for (String key : variables.keySet()) {
+                    log.debug("   '{}' = '{}'", key, variables.get(key));
+                }
+                log.debug("########################################################################");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        return variables;
     }
 }
