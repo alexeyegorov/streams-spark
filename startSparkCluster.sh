df833bfbe784a174eb514c9d58d0de1754893d93
Alexey Egorov <aegorov87@gmail.com>
2016-06-23 16:34:21 +0200

    updater script to start cluster with ws010

--- startSparkCluster.sh
+++ startSparkCluster.sh
@@ -11,7 +11,7 @@ username="egorov"
 spark_master="ls8ws007"
 spark_worker1="ls8ws008"
 spark_worker2="ls8ws009"
-spark_worker3="ls8olc09"
+spark_worker3="ls8ws010"
 filename=$2
 
 printf "\n\nUser: $username\nMaster: $spark_master\nWorker1: $spark_worker1\nWorker2: $spark_worker2\nWorker3: $spark_worker3\n\n"
@@ -29,17 +29,17 @@ if [[ $1 == "start" ]]; then
     # start Spark worker 1
     echo "Start worker 1..."
     slaveone_start_cmd="cd spark-worker1; $workerstartcmd"
-    start_master=`ssh "$username"@"$spark_worker1".cs.uni-dortmund.de "$slaveone_start_cmd"`
+    echo `ssh "$username"@"$spark_worker1".cs.uni-dortmund.de "$slaveone_start_cmd"`
 
     # start Spark worker 2
     echo "Start worker 2..."
     slavetwo_start_cmd="cd spark-worker2; $workerstartcmd"
-    start_master=`ssh "$username"@"$spark_worker2".cs.uni-dortmund.de "$slaveone_start_cmd"`
+    echo `ssh "$username"@"$spark_worker2".cs.uni-dortmund.de "$slavetwo_start_cmd"`
 
     # start Spark worker 3
     echo "Start worker 3..."
     slavethree_start_cmd="cd spark-worker3; $workerstartcmd"
-    start_master=`ssh "$username"@"$spark_worker3".cs.uni-dortmund.de "$slavethree_start_cmd"`
+    echo `ssh "$username"@"$spark_worker3".cs.uni-dortmund.de "$slavethree_start_cmd"`
 else
 
     stopcmd="./sbin/stop-all.sh;"
@@ -47,24 +47,24 @@ else
     # stop Spark master
     echo "Stop master..."
     master_start_cmd="cd spark-master; $stopcmd"
-    start_master=`ssh "$username"@"$spark_master".cs.uni-dortmund.de "$master_start_cmd"`
+    echo `ssh "$username"@"$spark_master".cs.uni-dortmund.de "$master_start_cmd"`
 
-
+    stopcmd="./sbin/stop-slave.sh;"
     workerstopcmd="$stopcmd cd work; pwd; rm -rf app-*; rm -rf driver-*;"
 
     # stop Spark worker 1
     echo "Stop worker 1..."
     slaveone_start_cmd="cd spark-worker1; $workerstopcmd"
-    start_master=`ssh "$username"@"$spark_worker1".cs.uni-dortmund.de "$slaveone_start_cmd"`
+    echo `ssh "$username"@"$spark_worker1".cs.uni-dortmund.de "$slaveone_start_cmd"`
 
     # stop Spark worker 2
     echo "Stop worker 2..."
     slavetwo_start_cmd="cd spark-worker2; $workerstopcmd"
-    start_master=`ssh "$username"@"$spark_worker2".cs.uni-dortmund.de "$slavetwo_start_cmd"`
+    echo `ssh "$username"@"$spark_worker2".cs.uni-dortmund.de "$slavetwo_start_cmd"`
 
     # stop Spark worker 3
     echo "Stop worker 3..."
     slavethree_start_cmd="cd spark-worker3; $workerstopcmd"
-    start_master=`ssh "$username"@"$spark_worker2".cs.uni-dortmund.de "$slavethree_start_cmd"`
+    echo `ssh "$username"@"$spark_worker3".cs.uni-dortmund.de "$slavethree_start_cmd"`
 fi
 
